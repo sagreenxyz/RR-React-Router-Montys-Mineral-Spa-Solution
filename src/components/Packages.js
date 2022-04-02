2f1023078d0444e772420ac0528e74134a86e055
Scott Green <sagreenxyz@gmail.com>
2022-04-02 09:53:10 -0500

    7. improvements to Packages component

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,23 +1,28 @@
-import ListGroup from 'react-bootstrap/ListGroup'
+import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
+import ListGroup from 'react-bootstrap/ListGroup'
+
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) =>
+        <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
         <Container>
-            <ListGroup>
-                <ListGroup.Item active>
-                    Lorem Ipsum
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    Here is another list item here
-                </ListGroup.Item>
-            </ListGroup>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
+                    {displayPackages}
+                </ListGroup>
+            </Card>
         </Container>
     )
 }
-
 
 
 
